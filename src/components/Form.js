bebb66570dcd77b8408bf6dbee437e8c0c82e5d4
Marwa Alaghbri <marwaalaghbry@gmail.com>
2020-09-25 05:41:08 +0000

    Automatically backed up by Learn

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,19 +26,7 @@ class Form extends React.Component {
         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
       </form>
 
-      <form>
-        <input
-          type="text"
-          name="firstName"
-          onChange={this.props.handleChange}
-          value={firstName}
-        />
-        <input
-          type="text"
-          name="lastName"
-          onChange={this.props.handleChange}
-          value={lastName} />
-      </form>
+      
 
     )
   }
